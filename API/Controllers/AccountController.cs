95876706bafc7b246801087367005618e661af4f
Pankaj Bhadane <pankajsbhadane@gmail.com>
2020-12-23 02:23:38 +0530

    End of section 11

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -7,6 +7,7 @@ using System.Text;
 using Microsoft.EntityFrameworkCore;
 using API.DTOs;
 using API.Interfaces;
+using System.Linq;
 
 namespace API.Controllers
 {
@@ -49,6 +50,7 @@ namespace API.Controllers
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
              var user = await _context.Users
+                 .Include(p => p.Photos)
                  .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
 
              if (user == null) return Unauthorized("Invalid username");
@@ -65,7 +67,8 @@ namespace API.Controllers
             return new UserDto
             {
                Username = user.UserName,
-               Token = _tokenService.CreateToken(user)
+               Token = _tokenService.CreateToken(user),
+               PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
             };
         }
 
